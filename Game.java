2c7727b1d11c9921f1475eaed86352a032dda94e
Victoria Dix <71494865+VictoriaDix@users.noreply.github.com>
2021-04-06 11:49:33 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,17 @@
 public class Game extends Product {
     private int ageRate;
+    
+    
 
 
-    public int getAgeRate(){return ageRate;}
-    public void setAgeRate(int ageRate){this.ageRate=ageRate;}
+    public int getAgeRate(){
+        return ageRate;
+    }
+    public void setAgeRate(int ageRate){
+        this.ageRate=ageRate;
+    }
 
+   
     @Override
     public String getDescription() {
         return super.getDescription()+ ",Age rate recommend : "+ ageRate;
