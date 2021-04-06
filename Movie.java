1bff2bf6b574f0b64c77836e1c970857308a58b0
Victoria Dix <71494865+VictoriaDix@users.noreply.github.com>
2021-04-06 11:53:35 +0200

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -3,8 +3,12 @@ public class Movie extends Product {
     private String director;
 
 
-    public String getDirector(){return director;}
-    public void setDirector(String director){this.director=director;}
+    public String getDirector(){
+        return director;
+    }
+    public void setDirector(String director){
+        this.director=director;
+    }
 
 
     @Override
