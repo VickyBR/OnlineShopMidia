23ca9cd2b815345988d24054df6375ffee06abaa
Victoria Dix <71494865+VictoriaDix@users.noreply.github.com>
2021-04-06 11:52:57 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,8 +3,13 @@ public class Book extends Product{
 
 
 
-    public String getAuthor(){return author;}
-    public void setAuthor(String author){this.author=author;}
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+    
 
     @Override
     public String getDescription() {
