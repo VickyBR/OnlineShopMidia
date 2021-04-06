e6a93a0e9b36798ed3410c6c030fe1b452f3bae2
Victoria Dix <71494865+VictoriaDix@users.noreply.github.com>
2021-04-06 11:55:15 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -8,18 +8,38 @@ public abstract class Product {
 
 
 
-    public String getManufacturer(){return manufacturer;}
-    public void setManufacturer(String manufacturer){this.manufacturer=manufacturer;}
+    public String getManufacturer(){
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer){
+        this.manufacturer=manufacturer;
+    }
+    
+    
+   
 
-    public String getTitle(){return title;}
-    public void setTitle(String title){this.title=title;}
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    
+    
 
-    public String  getProductNumber(){return productNumber;}
-    public void setProductNumber(String productNumber){this.productNumber=productNumber;}
+    public String  getProductNumber(){
+        return productNumber;
+    }
+    public void setProductNumber(String productNumber){
+        this.productNumber=productNumber;
+    }
+    
+    
 
     public String getDescription(){
         return " Manufacturer : "+ manufacturer + ", Title  : "+ title
-+ ", Product number  : "+ productNumber; }
++ ", Product number  : "+ productNumber; 
+    }
 
 
 
